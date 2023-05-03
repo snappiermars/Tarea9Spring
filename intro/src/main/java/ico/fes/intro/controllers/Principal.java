5fac4edcb122321a524ed64aa0706eeb503ffe0c
snappiermars <snappiermars848@gmail.com>
2023-05-02 19:43:27 -0600

    Agregamos la clase Empleado y modificamos el html para que muestre los datos del nombre y departamento del empleado

--- intro/src/main/java/ico/fes/intro/controllers/Principal.java
+++ intro/src/main/java/ico/fes/intro/controllers/Principal.java
@@ -1,14 +1,18 @@
 package ico.fes.intro.controllers;
 
+import ico.fes.intro.model.Empleado;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Date;
 
 @Controller
 public class Principal {
     @GetMapping("/inicio")
     public String getInicio(Model model){
-        model.addAttribute("nombre", "José");
+        Empleado emp = new Empleado(1,"Jose Sosa", 23000,"Desarrollo", "12/12/2000");
+        model.addAttribute("empleado", emp);
         return "index";
     }
 
---
+++ intro/src/main/java/ico/fes/intro/model/Empleado.java
@@ -0,0 +1,20 @@
+package ico.fes.intro.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Empleado {
+    private int id;
+    private String nombre;
+    private double sueldo;
+    private String departamento;
+    private String fechaNacimiento;
+}
